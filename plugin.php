50c56c4484be19f349daa4bb1dbbc652b4d22a0a
Brad Daily <brad@slideshowpro.net>
2012-10-19 13:30:03 -0500

    Init

---
+++ plugin.php
@@ -0,0 +1,30 @@
+<?php
+
+class KokenHtmlInjector extends KokenPlugin {
+
+	function __construct()
+	{
+		$this->register_output_filter('before_closing_head', 'head');
+		$this->register_output_filter('before_closing_body', 'foot');
+	}
+
+	function head()
+	{
+
+		if (isset($this->data->header))
+		{
+			return $this->data->header;	
+		}
+
+	}
+
+	function foot()
+	{
+
+		if (isset($this->data->footer))
+		{
+			return $this->data->footer;	
+		}
+
+	}
+}
